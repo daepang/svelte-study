387cc470092b0b05384ebb5369752f3127d8b14f
daepang <dev.daepang@gmail.com>
2023-03-22 15:40:49 +0900

    Header.svelte 수정

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,11 +19,11 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/info' ? 'page' : undefined}>
+				<a href="/info">Info</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname === '/settings' ? 'page' : undefined}>
+				<a href="/settings">Settings</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
