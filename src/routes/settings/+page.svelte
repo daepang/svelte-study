4ce289cc634cf0505ccee22629e3d0a6c51d84f9
daepang <dev.daepang@gmail.com>
2023-03-27 14:10:38 +0900

    소스 정리

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,11 +1,5 @@
 <script>
     import Counter from '../Counter.svelte';
-
-    const imageUrl = 'https://picsum.photos/100/100';
-
-    const followingCnt = 0;
-    const followerCnt = 0;
-
 </script>
 
 <section>
